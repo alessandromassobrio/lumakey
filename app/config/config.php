d3009aee9cbb31ab74d35ff6c13b8f58d4f3a238
alessandro <alessandro.massobrio@gmail.com>
2018-09-17 14:23:53 +0200

    Add config and autoload core libs

---
+++ app/config/config.php
@@ -0,0 +1,7 @@
+<?php
+  // App Root
+  define('APPROOT', dirname(dirname(__FILE__)));
+  // URL Root
+  define('URLROOT', 'http://localhost/osmvc');
+  // Site Name
+  define('SITENAME', 'A basic MVC Framework');
